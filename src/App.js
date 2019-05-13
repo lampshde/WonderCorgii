7483d9aa9e28062b8f7afc9939a8d90e6c61ae65
John Randall <john.randall.dev@gmail.com>
2019-05-12 20:17:29 -0400

    More CD Test, built our the nav a bit more. Going to write some content soon

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Nav from './Nav.js'
 
+const navItems = ['INTRO','SKILLS','BLOG','CONTACT']
 function App() {
   return (
     <div className="App">
-      <Nav></Nav>
-      <div className="title animated fadeInDown" >WonderCorgii</div>
+      <Nav navItems={navItems}></Nav>
+      <div className="title animated fadeInDown" >JOHNRANDALL</div>
+      <div className="sub-title animated fadeInDown">Fullstack Web Developer</div>
     </div>
   );
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import './Nav.css';
 
-function Nav() {
-    return (
-        <nav className="animated fadeInRight">
-            <div className="nav-item">
-                <div className="nav-item-text">INTRO</div>
-                <div className="nav-item-index">01</div>
-            </div>
-            <div className="nav-item">
-                <div className="nav-item-text">APPS</div>
-                <div className="nav-item-index">02</div>
-            </div>
-            <div className="nav-item">
-                <div className="nav-item-text">BLOG</div>
-                <div className="nav-item-index">03</div>
-            </div>
-            <div className="nav-item">
-                <div className="nav-item-text">CONTACT</div>
-                <div className="nav-item-index">04</div>
-            </div>
-        </nav>
-    );
+class Nav extends React.Component{
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedIndex: -1
+        }
+
+        this.selectNavItem = this.selectNavItem.bind(this);
+    }
+    render(){
+        return (
+            <nav className={"animated fadeInRight " + (this.state.selectedIndex >= 0 ? 'minimized' : '')}>
+                <div className="nav-links-wrapper" style={this.state.selectedIndex >= 0 ? {transform: `translateY(${-40*this.state.selectedIndex}px)`} : {}}>
+                    {this.props.navItems.map((value,index) => {
+                        return <div className={"nav-item " + (this.state.selectedIndex === index ? 'selected' : '')} onClick={()=>(this.selectNavItem(index))}>
+                            <div className="nav-item-text">{value}</div>
+                            <div className="nav-item-index">0{index}</div>
+                        </div>
+                    })}
+                </div>
+            </nav>
+        );
+    }
+
+    selectNavItem(index){
+        if(index === this.state.selectedIndex){
+            this.setState({selectedIndex: -1})
+        }
+        else{
+            this.setState({selectedIndex: index});
+        }
+    }
 }
 
 export default Nav;
